5866cb5bde4f6b256432742abb592531ec65980a
tharun <tharundadithota@gmail.com>
2025-06-04 11:41:04 +0530

    hello world started

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo “Trump: Hey Musk, How are You?”
+echo “Musk: Hello Trump. I am fine, how are you doing?”
+echo “Trump: I’m fine too, what's up?”
+echo “Musk: Nothing, just going for a Mars trip, will you join?”
+echo “Trump:  Sorry, you carry on! I will join once you return back”.
