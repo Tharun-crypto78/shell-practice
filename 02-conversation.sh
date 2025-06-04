27e45ea3253931146004478bb0dba2df91dddec3
tharun <tharundadithota@gmail.com>
2025-06-04 21:50:52 +0530

    “variables”

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo “Trump: Hey Musk, How are You?”
-echo “Musk: Hello Trump. I am fine, how are you doing?”
-echo "Trump: I am fine too. What's up?"
-echo "Musk: Nothing, just going to Mars now, will you come"
-echo "Trump: Sorry, you carry on! I will join once you come back"
+echo "Trump:: Hey Musk, How are you?"
+echo "Musk:: Hello Trump, I am fine. How are you doing"
+echo "Trump: I am fine too.What's up?"
+echo "Musk:: Nothing, just going to Mars now, will you come?"
+echo "Trump:: Sorry, you carry on! I will come once you come back"
 
