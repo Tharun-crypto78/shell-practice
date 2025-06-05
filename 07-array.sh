1af137ddb2f9a1426d00353e277d4b72a2550070
tharun <tharundadithota@gmail.com>
2025-06-05 07:00:10 +0530

    special vars

--- 07-array.sh
+++ 07-array.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MOVIES=("Guntur Karam" "American Hunt" "Malli Rava" "HIT")
+MOVIES=("GunturKaram" "AmericanHunt" "MalliRava" "HIT")
 
 echo "First Movie: ${MOVIES[0]}"
 echo "Last Movie: ${MOVIES[3]}"
