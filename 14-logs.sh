35758a63a69477df03ca9124c3a78650c8f16451
tharun <tharundadithota@gmail.com>
2025-06-11 21:52:46 +0530

    colors

--- 14-logs.sh
+++ 14-logs.sh
@@ -11,11 +11,11 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" &>>$LOG_FILE
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE 
 
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R ERROR: please run this script with root access $N" &>>$LOG_FILE
+    echo -e "$R ERROR: please run this script with root access $N" | tee -1 $LOG_FILE
     exit 1 #if you want to manually exit then give other than 0, upto 127
 else
     echo "You are running with the root access" &>>$LOG_FILE
@@ -24,45 +24,45 @@ fi
 VALIDATE(){
     if [ $? -eq 0 ]
     then 
-        echo -e "Installing $2 is $G SUCCESS $N" &>>$LOG_FILE
+        echo -e "Installing $2 is $G SUCCESS $N" | tee -a $LOG_FILE 
     else 
-        echo -e "Installing $2 is $R FAILURE $N" &>>$LOG_FILE
+        echo -e "Installing $2 is $R FAILURE $N" | tee -a $LOG_FILE 
         exit 1
     fi
 }
 
-dnf list installed mysql &>>$LOG_FILE
+dnf list installed mysql &>>$LOG_FILE 
 
 if [ $? -ne 0 ]
 then
-    echo "MySQL is not installed..... is about to install it" &>>$LOG_FILE
-    dnf install mysql -y &>>$LOG_FILE
+    echo "MySQL is not installed..... is about to install it" | tee -a $LOG_FILE 
+    dnf install mysql -y &>>$LOG_FILE  
     # we can check the exit status
     VALIDATE $? "MySQL"
 else  
-    echo -e "Nothing to do MySQL... $Y is already installed $N." &>>$LOG_FILE
+    echo -e "Nothing to do MySQL... $Y is already installed $N." | tee -a $LOG_FILE 
 fi
 
-dnf list installed python3 &>>$LOG_FILE
+dnf list installed python3 &>>$LOG_FILE  
 
 if [ $? -ne 0 ]
 then
-    echo "python3 is not installed..... is about to install it" &>>$LOG_FILE
-    dnf install python3 -y &>>$LOG_FILE
+    echo "python3 is not installed..... is about to install it" | tee -a $LOG_FILE 
+    dnf install python3 -y &>>$LOG_FILE  
     # we can check the exit status
     VALIDATE $? "python3"
 else  
-    echo -e "Nothing to do python3.... $Y is already installed $N." &>>$LOG_FILE
+    echo -e "Nothing to do python3.... $Y is already installed $N." | tee -a $LOG_FILE 
 fi
 
-dnf list installed nginx &>>$LOG_FILE
+dnf list installed nginx &>>$LOG_FILE  
 
 if [ $? -ne 0 ]
 then
-    echo "nginx is not installed..... is about to install it" &>>$LOG_FILE
-    dnf install nginx -y &>>$LOG_FILE
+    echo "nginx is not installed..... is about to install it" | tee -a $LOG_FILE 
+    dnf install nginx -y &>>$LOG_FILE  
     # we can check the exit status
     VALIDATE $? "nginx"
 else  
-    echo -e "Nothing to do nginx.... $Y is already installed $N " &>>$LOG_FILE
+    echo -e "Nothing to do nginx.... $Y is already installed $N " | tee -a $LOG_FILE 
 fi
