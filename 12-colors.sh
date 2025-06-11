30af76101d609a7e8d5020d4a55b19835e4c65d1
tharun <tharundadithota@gmail.com>
2025-06-11 06:41:28 +0530

    colors

--- 12-colors.sh
+++ 12-colors.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo -e "\e[31m Hello Colors"
+echo -e "\e[31m Hello Colors \e[0m" #here we need to end the color code to that particular line by giving [0m, otherwise it will color the remaining lines also.
 
 echo "Hello No Colors"
