e81e3d712501fa331b7d01e84bcff5c2c7e233bc
tharun <tharundadithota@gmail.com>
2025-06-27 18:03:43 +0530

    Calling the Scripts

--- script-1.sh
+++ script-1.sh
@@ -4,4 +4,4 @@ GREETING="Hello, Good Morning"
 echo "$GREETING"
 echo "PID of SCRIPT-1: $$"
 
-./script-2.sh #here . represents current directory
+source ./script-2.sh #here . represents current directory
