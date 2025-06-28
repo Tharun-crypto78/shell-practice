4c99b933584bb81461b99399671fbad986add488
tharun <tharundadithota@gmail.com>
2025-06-28 11:07:49 +0530

    user and cart setup

--- 17-set.sh
+++ 17-set.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e  # usually shell-scripting proceeds even the error occurs and if we want to stop that then use "set -e"
+
 echo "Hi, Good Morning"
 echoooooo "Hello, this will be error"
 echo "Hello, Morning"
